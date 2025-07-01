7fe8ca5d96c0d15e661266e7a77151a6c5dc9ae8
Beom-Soo Kim <kbeom00@gmail.com>
2025-07-01 22:35:30 +0900

    in parser.c, read line by line from file. delete newline char and delete comment

---
+++ README.md
@@ -0,0 +1,5 @@
+# bum_riscv_assembler
+
+**bum_riscv_assembler** is a RISC-V assembler in C.
+This is a project for learning.
+
---
+++ example.s
@@ -0,0 +1,11 @@
+    addi x1, x0, 5      # x1 = 5
+    addi x2, x0, 10     # x2 = 10
+    add  x3, x1, x2     # x3 = x1 + x2
+
+loop:
+    beq  x3, x0, end
+    addi x3, x3, -1
+    jal  x0, loop
+
+end:
+    nop
